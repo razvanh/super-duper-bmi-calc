e26858f7683680031fb5a385a87440e973661f45
Horeanga, Razvan <razvan-horeanga@uiowa.edu>
2017-05-08 17:01:00 -0500

    basic html/css/js structure

---
+++ src/bmicalc.js
@@ -0,0 +1,25 @@
+var BMIClaclModel = {
+
+}
+
+var BMICalcController = {
+
+}
+
+var BMICalcImperialFormView = {
+
+	init : function (){
+		//Pointers to DOM Elements
+
+		//Event Listeners
+	},
+
+	render: function () {
+		//update values
+	}
+
+}
+
+var BMICalcResultsView = {
+
+}
