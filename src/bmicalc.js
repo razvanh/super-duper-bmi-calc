a83e09176ccb398535e6db453cf267c5ad9647ca
Horeanga, Razvan <razvan-horeanga@uiowa.edu>
2017-05-09 14:22:07 -0500

    dinamically generating the html for the calculator

--- src/bmicalc.js
+++ src/bmicalc.js
@@ -7,27 +7,77 @@ var BMICalcController = {
 	init: function (){
 
 		//Initialize views
-
-		BMICalcImperialFormView.init();
-		BMICalcResultsView.init();
+		BMICalcView.init();
 	}
 
 }
 
-var BMICalcImperialFormView = {
 
-	init : function (){
-		//Pointers to DOM Elements
+var BMICalcView = {
+	init: function (){
+		this.calcContainer = document.getElementById('bmicalc');
 
-		//Event Listeners
+		this.imperialFormHtml = `
+		<div id="bmicalc-form">
+			<form action="#" id="bmicalc-imperial">
+				<div class="bmicalc-heading">
+					<h2>Body mass index (BMI)</h2>
+					<p class="bmicalc-subhead">A measure of body fat in adults</p>	
+				</div>
+				
+				<fieldset>
+					<legend>Weight</legend>
+					<p>
+						<input type="number" required id="bmicalc-weight-imperial">
+						<label for="bmicalc-weight-imperial">lb</label>
+					</p>
+				</fieldset>
+				
+				<fieldset>
+					<legend>Height</legend>
+
+					<p>
+						<input type="number" required id="bmicalc-height-ft">
+						<label for="bmicalc-height-ft">ft</label>
+					</p>
+
+					<p>
+						<input type="number" required id="bmicalc-height-in">
+						<label for="bmicalc-height-in">in</label>
+					</p>
+				</fieldset>
+
+			</form>
+		</div>	
+		`;
+
+		this.resultsHTML = `
+			<div id="bmicalc-result">
+				<div id="bmicalc-number">
+					0
+				</div>
+
+				<div id="bmicalc-resources">
+					
+				</div>
+			</div>
+		`;
+
+		this.render();
+
+		document.addEventListener('change',function(e){
+			if (e.target && e.target.id == 'bmicalc-weight-imperial') {
+				console.log(e.target.id);
+			}
+
+		 });
+
 	},
 
-	render: function () {
-		//update values
+	render: function(){
+		this.calcContainer.insertAdjacentHTML('afterbegin', this.imperialFormHtml) ;
+		this.calcContainer.insertAdjacentHTML('beforeend', this.resultsHTML);
 	}
-
 }
 
-var BMICalcResultsView = {
-
-}
+BMICalcController.init();
