3cb64f5e740a8dfc4ac7a09ae23fdb2227d19585
Razvan Horeanga <razvan-horeanga@uiowa.edu>
2018-01-30 14:10:40 -0600

    added test for calculateBMI return type

--- src/bmicalc.test.js
+++ src/bmicalc.test.js
@@ -1,5 +1,8 @@
 const BMICalcController = require('./bmicalc');
 
+test('calculateBMI returns a number', () => {
+    expect(typeof BMICalcController.calculateBMI('standard',{weight:155,ft:5,in:9})).toBe("number");
+});
 test('correctly calculates BMI when standard units are used', () => {
     expect(BMICalcController.calculateBMI('standard',{weight:155,ft:5,in:9})).toBe(22.9);
 });
